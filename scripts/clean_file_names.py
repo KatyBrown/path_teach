a15fc1eb910c724bcdbed03a4c49fa957c3a2564
Katy Brown <kab84@cam.ac.uk>
2025-07-15 14:10:54 +0100

    documentation

--- scripts/clean_file_names.py
+++ scripts/clean_file_names.py
@@ -163,9 +163,18 @@ def choose_folder_qt():
 
 
 def get_contents(folder, log):
+    '''
+    Find the contents of the main folder and list all the subfolders
+    it contains. Prints an error if any of the files in the folder are not
+    directories.
+    '''
+    # Look up the contents of the folder
     files = glob.glob(f"{folder}/*")
+    # Make an empty list to store the folder names
     subdirs = []
+    # Iterate through the files
     for file in files:
+        # Add directories (folders) to the list
         if os.path.isdir(file):
             subdirs.append(file)
         else:
@@ -174,80 +183,126 @@ def get_contents(folder, log):
 
 
 def copy_files(list_subfolds, out_folder, students, log):
+    '''
+    Copies and rename the files or raises and logs an appropriate error.
+    '''
+    # Make a dictionary of the student IDs from the lookup table. 
+    # Keys are surnames and values are anonymous IDs
+    # Allows for duplicate names
     anonD = dict()
     for lastname, ID in zip(students['lastname_clean'],
                             students['Anon']):
         anonD.setdefault(lastname, set())
         anonD[lastname].add(ID)
 
+    # Look through every subfolder
     for subfold in list_subfolds:
+        # Output current folder name to screen
         print(f"Processing folder {subfold}")
+
+        # Extract the file name and then the surname, remove special characters
         nam = subfold.split("/")[-1]
+        # Remove initials
         pattern = r'^[A-Z](?:\.[A-Z])*\.'
         clean_nam = re.sub(pattern, "", nam)
+        # Remove special characters
         lastname = clean_string(clean_nam.split("_")[0])
 
+        # List the contents of the current subfolder
         contents = os.listdir(subfold)
         x = 0
         file_found = None
+        # Look for a file which has the folder name as the first part of
+        # the filename
+        # e.g. B.Ahmed_30192844_assignsubmission_file
+        # to B.Ahmed_30192844_assignsubmission_file_7859K.docx
         for c in contents:
             if clean_nam in c:
                 file_found = c
+                # Add to x only if a matching file is found
                 x += 1
         if x < 1:
+            # Error if no matching files are found
             process_error(6, log, vals=subfold)
             continue
         elif x > 1:
+            # Error if more than one matching file is found
             process_error(7, log, vals=subfold)
             continue
         else:
+            # If exactly one matching file is found
             id_from_file = file_found.split("_")[-1].split(".")[0]
             y = 0
+            # Check the ID for this student in the student table
             if lastname in set(students['lastname_clean']):
                 IDs = anonD[lastname]
+                # Check it matches
                 if id_from_file in IDs:
                     y += 1
+                    # Warn if there is more than one student with the
+                    # same name
                     if len(IDs) > 1:
                         process_warn(1, log, val=[f"{subfold}/{file_found}",
                                                   lastname,
                                                   id_from_file])
             else:
+                # Error if the student name is not in the table
                 process_error(8, log, vals=[f"{subfold}/{file_found}",
                                             lastname, ""])
                 continue
             if y == 0:
+                # Error if the ID in the filename doesn't match the ID
+                # in the table
                 IDD = ",".join(IDs)
                 process_error(9, log, vals=[f"{subfold}/{file_found}",
                                             id_from_file, IDD])
                 continue
             else:
+                # If everything is fine, extract the file suffix e.g. .doc
                 suffix = file_found.split(".")[-1]
+                # Generate the new file name
                 newname = f"{out_folder}/{id_from_file}.{suffix}"
+                # Copy the file to the new name
                 shutil.copy(f"{subfold}/{file_found}", newname)
 
 
 def main():
+    '''
+    Main function to run the code
+    '''
+    # Ask the user for the input folder, output folder and student table
     in_folder, out_folder, student_tab = choose_folder_qt()
+    # If there is no output folder, raise an error - can't log anything
+    # as there's no folder to put it in
     if not os.path.exists(out_folder):
         process_error(1, log=None)
 
+    # Create a string of the date and time - to name the log file
     now = datetime.now()
     timestamp = now.strftime("%Y%m%d-%H:%M:%S")
+    # Create the log file
     log = open(f"{out_folder}/logfile_{timestamp}.txt", "w")
 
+    # If the input folder or student table don't exist, raise errors
     if not os.path.exists(in_folder):
         process_error(2, log)
     if not os.path.exists(student_tab):
         process_error(3, log)
 
+    # Read the student table
     students = pd.read_csv(student_tab, sep="\t").drop_duplicates()
+    # If there are duplicate student IDs with different names, raise an error
     if len(set(students['Anon'])) != len(students):
         dups = set(students['Anon'][students['Anon'].duplicated()])
         process_error(4, log, vals=dups)
+    # Remove special characters from student last names
     students['lastname_clean'] = [
         clean_string(x) for x in students['LastName']]
+    # List all the subfolders in in_folder
     list_subfolds = get_contents(in_folder, log)
+    # Copy and rename the files
     copy_files(list_subfolds, out_folder, students, log)
+    # Close the log file
     log.close()
 
 
