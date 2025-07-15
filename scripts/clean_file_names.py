3ee4f3994d96d21018c9118978d8a258bcd04c97
Katy Brown <kab84@cam.ac.uk>
2025-07-15 13:44:29 +0100

    documentation and hopefully fixed qt bug

--- scripts/clean_file_names.py
+++ scripts/clean_file_names.py
@@ -10,10 +10,18 @@ import shutil
 
 
 def remove_non_ascii(text):
+    '''
+    Removes non-ascii characters from a string - just used so special
+    characters in names don't break it.
+    '''
     return ''.join(i for i in text if ord(i) < 128)
 
 
 def clean_string(string):
+    '''
+    Remove punctutation etc from a string - used to clean student names -
+    for comparison with the table only.
+    '''
     string = unidecode.unidecode(string)
     string = re.sub(r'[^\w\s]', "_", string)
     string = remove_non_ascii(string)
@@ -23,6 +31,10 @@ def clean_string(string):
 
 
 def get_basic_error_dict():
+    '''
+    Errors which prevent the tool from running - no input, no output,
+    repeated IDs.
+    '''
     return {
         1: 'Output folder must be specified',
         2: 'Input folder must be specified',
@@ -31,6 +43,10 @@ def get_basic_error_dict():
 
 
 def get_folder_error_dict(folder):
+    '''
+    Errors on the contents of a folder - internal file not found or too many
+    files found.
+    '''
     return {
         5: f'{folder}\tNot processed - Not a directory',
         6: f'{folder}\tNot processed - No matching files found',
@@ -39,6 +55,10 @@ def get_folder_error_dict(folder):
 
 
 def get_file_error_dict(vals):
+    '''
+    Errors affecting individual files - students not found in the student
+    list or mismatched IDs.
+    '''
     return {
         8: f'{vals[0]}\tNot processed \
 - last name {vals[1]} not found in student list',
@@ -48,6 +68,10 @@ def get_file_error_dict(vals):
 
 
 def get_file_warn_dict(filepath, val1, val2):
+    '''
+    Warning for an individual file - if there is more than one student with
+    the same surname.
+    '''
     return {
         1: f'{filepath}\t\
 Student name {val1} has more than one entry, using {val2}'
@@ -55,62 +79,85 @@ Student name {val1} has more than one entry, using {val2}'
 
 
 def process_error(n, log, vals=None):
+    '''
+    Generates the error message, logs it and exits or continues as needed.
+    Only errors 1-4 will raise an error - otherwise it just continues to the
+    next file.
+    '''
     if n <= 4:
+        # Get the error message dictionary
         errors = get_basic_error_dict()
+        # Access the relevant error
         message = errors[n]
         if n <= 3:
+            # Adds consitent formatting to the error messages
             long_message = f"ERROR - {message}\tERROR {n}\n"
+            # Write to the log file and close it
             if log:
                 log.write(f"{long_message}\n")
-                print(long_message.strip())
                 log.close()
+            # Raise an error and show the error message
             raise RuntimeError(long_message)
         else:
+            # List of IDs involved in the error
             ids = ",".join(vals)
+            # Adds consitent formatting to the error messages
             long_message = f"ERROR - {message} {ids}\tERROR {n}\n"
+            # Write to the log file and close it
             log.write(f"{long_message}\n")
-            print(long_message.strip())
             log.close()
+            # Raise an error and show the error message
             raise RuntimeError(long_message)
-    elif n == 5 or n == 6 or n == 7:
-        errors = get_folder_error_dict(vals)
+    elif n >= 5:
+        if n == 5 or n == 6 or n == 7:
+            # Get the folder error message dictionary
+            errors = get_folder_error_dict(vals)
+        else:
+            # Get the file error message dictionary
+            errors = get_file_error_dict(vals)
+        # Access the relevant error
         message = errors[n]
+        # Format an error message
         long_message = f"{message}\tERROR {n}\n"
-        print(repr(long_message))
-        print(long_message.strip())
-        log.write(long_message)
-    else:
-        errors = get_file_error_dict(vals)
-        message = errors[n]
-        long_message = f"{message}\tERROR {n}\n"
+        # Show the error and write it to the log file
         print(long_message.strip())
         log.write(long_message)
 
 
 def process_warn(n, log, val=None):
+    '''
+    Generates a warning message - the file is still processed but a warning
+    is written to the output file.
+    '''
+    # Get the warning message dictionary
     warns = get_file_warn_dict(val[0], val[1], val[2])
+    # Find and format the warning message
     message = warns[n]
     long_message = f"{message}\tWARN {n}\n"
+    # Show the warning and write it to the log file
+    print(long_message.strip())
     log.write(long_message)
 
 
 def choose_folder_qt():
     '''
-    Opens system dialogues to find the input and output directories
+    Opens system dialogues to find the input and output directories and
+    the student lookup table.
     '''
     # Initiates the PyQt object needed to use system diaglogues
-    QApplication(sys.argv)
+    app = QApplication(sys.argv)
 
     # Ask for and store the input folder
     in_folder = QFileDialog.getExistingDirectory(
-        None, "Select folder containing the student documents")
+        None, "Select folder containing student documents")
 
     # Ask for and store the output folder
     out_folder = QFileDialog.getExistingDirectory(
-        None, "Select or create a different folder for the clean files")
+        None, "Create a different folder for the clean files")
 
+    # Ask for and store the student lookup table
     student_tab = QFileDialog.getOpenFileName(
-        None, "Select the table containing student names and IDs")[0]
+        None, "Select table of student names and IDs")[0]
 
     return in_folder, out_folder, student_tab
 
